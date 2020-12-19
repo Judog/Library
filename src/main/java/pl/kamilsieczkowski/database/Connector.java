398eabec3fd192393745240ccc84339696df8cad
Judog <kamilsieczkowski85@gmail.com>
2020-12-19 17:32:30 +0100

    nullpointer fixed

--- src/main/java/pl/kamilsieczkowski/database/Connector.java
+++ src/main/java/pl/kamilsieczkowski/database/Connector.java
@@ -15,22 +15,16 @@ public class Connector {
         String query = "SELECT * FROM library_users.users WHERE username='";
         String enteredQuery = query + enteredLogin + "'"; // select row of entered login, from users database
         User user = new User(enteredLogin, null, null);
-        Connection con = DriverManager.getConnection(null, null, null);
-        try {
-            con = DriverManager.getConnection(SERVER_URL, SERVER_USER, SERVER_PASSWORD);
-            PreparedStatement pst = con.prepareStatement(enteredQuery);
-            ResultSet resultSet = pst.executeQuery();
-            while (resultSet.next()) {
-                String username = resultSet.getString("username");
-                String password = resultSet.getString("password");
-                String privilege = resultSet.getString("privilege");
-                user = new User(username, password, privilege);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            con.close();
+        Connection con = DriverManager.getConnection(SERVER_URL, SERVER_USER, SERVER_PASSWORD);
+        PreparedStatement pst = con.prepareStatement(enteredQuery);
+        ResultSet resultSet = pst.executeQuery();
+        while (resultSet.next()) {
+            String username = resultSet.getString("username");
+            String password = resultSet.getString("password");
+            String privilege = resultSet.getString("privilege");
+            user = new User(username, password, privilege);
         }
+
         return user;
     }
 }
