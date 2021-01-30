ab747a95ee267494ddf201b3a1cf81a0fc34f9ec
Judog <kamilsieczkowski85@gmail.com>
2021-01-31 00:11:02 +0100

    String fixed

--- src/main/java/pl/kamilsieczkowski/Main.java
+++ src/main/java/pl/kamilsieczkowski/Main.java
@@ -4,13 +4,13 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import pl.kamilsieczkowski.utils.Window;
 
-import static pl.kamilsieczkowski.constants.Texts.LOGIN;
+import static pl.kamilsieczkowski.constants.Texts.IN_LIBRARY;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         Window window = new Window();
-        window.openNewWindow("/loginWindow.fxml", LOGIN);
+        window.openNewWindow("/loginWindow.fxml", IN_LIBRARY);
     }
 
     public static void main(String[] args) {
